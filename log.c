88040dcc724249764e8fd70c9016d46160d713d7
LiorFrenkel1 <liorfr5002@gmail.com>
2025-06-23 04:21:17 +0300

    Add most of log.c implementation

--- log.c
+++ log.c
@@ -1,21 +1,53 @@
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "log.h"
+
+typedef struct LogDataNode {
+    char* logData;
+    struct LogDataNode* next;
+}   LogDataNode;
 
 // Opaque struct definition
 struct Server_Log {
-    // TODO: Implement internal log storage (e.g., dynamic buffer, linked list, etc.)
+    LogDataNode* head;
+    int readersInside;
+    int writersInside;
+    int writersWaiting;
+    pthread_mutex_t mutexLock;
+    pthread_cond_t readAllowed;
+    pthread_cond_t writeAllowed;
 };
 
 // Creates a new server log instance (stub)
 server_log create_log() {
-    // TODO: Allocate and initialize internal log structure
-    return (server_log)malloc(sizeof(struct Server_Log));
+    server_log serverLog = (server_log)malloc(sizeof(struct Server_Log));
+    serverLog->head = NULL;
+    serverLog->readersInside = 0;
+    serverLog->writersInside = 0;
+    serverLog->writersWaiting = 0;
+    pthread_mutex_init(&serverLog->mutexLock, NULL);
+    pthread_cond_init(&serverLog->readAllowed, NULL);
+    pthread_cond_init(&serverLog->writeAllowed, NULL);
+    return serverLog;
 }
 
 // Destroys and frees the log (stub)
 void destroy_log(server_log log) {
-    // TODO: Free all internal resources used by the log
+    if (!log) {
+        return;
+    }
+    pthread_mutex_destroy(&log->mutexLock);
+    pthread_cond_destroy(&log->readAllowed);
+    pthread_cond_destroy(&log->writeAllowed);
+    LogDataNode* current = log->head;
+    LogDataNode* temp;
+    while (current != NULL) {
+        temp = current->next;
+        free(current->logData);
+        free(current);
+        current = temp;
+    }
     free(log);
 }
 
@@ -23,13 +55,34 @@ void destroy_log(server_log log) {
 int get_log(server_log log, char** dst) {
     // TODO: Return the full contents of the log as a dynamically allocated string
     // This function should handle concurrent access
-
-    const char* dummy = "Log is not implemented.\n";
-    int len = strlen(dummy);
-    *dst = (char*)malloc(len + 1); // Allocate for caller
-    if (*dst != NULL) {
-        strcpy(*dst, dummy);
+    pthread_mutex_lock(&log->mutexLock);
+    while (log->writersWaiting >= 1 || log->writersInside >= 1) {
+        pthread_cond_wait(&log->readAllowed, &log->mutexLock);
     }
+    log->readersInside++;
+
+    int len = 0;
+    LogDataNode* current = log->head;
+    while (current != NULL) {
+        len += (int)strlen(current->logData);
+        current = current->next;
+    }
+    *dst = (char*)malloc(len + 1); // Allocate for caller
+    *dst[0] = '\0';
+    if (*dst != NULL) {
+        current = log->head;
+        while (current != NULL) {
+            strcat(*dst, current->logData);
+            current = current->next;
+        }
+    }
+
+    pthread_mutex_lock(&log->mutexLock);
+    log->readersInside--;
+    if (log->readersInside == 0) {
+        pthread_cond_signal(&log->writeAllowed);
+    }
+    pthread_mutex_unlock(&log->mutexLock);
     return len;
 }
 
