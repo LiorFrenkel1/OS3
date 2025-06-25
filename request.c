17d126f86ae324bc03e8bcb514bef2935c58bfb6
yairyanni <97431915+yairyanni@users.noreply.github.com>
2025-06-25 19:27:42 +0300

    initialize buffers

--- request.c
+++ request.c
@@ -1,6 +1,6 @@
 //
 // request.c: Does the bulk of the work for the web server.
-// 
+//
 
 #include "segel.h"
 #include "request.h"
@@ -11,9 +11,8 @@ int append_stats(char* buf, threads_stats t_stats, struct timeval arrival, struc
     offset += sprintf(buf + offset, "Stat-Req-Arrival:: %ld.%06ld\r\n",
                       arrival.tv_sec, arrival.tv_usec);
 
-    double dispatch_delay = (dispatch.tv_sec - arrival.tv_sec) +
-                            (dispatch.tv_usec - arrival.tv_usec) / 1000000.0;
-    offset += sprintf(buf + offset, "Stat-Req-Dispatch:: %.6f\r\n", dispatch_delay);
+    offset += sprintf(buf + offset, "Stat-Req-Dispatch:: %ld.%06ld\r\n",
+                      dispatch.tv_sec, dispatch.tv_usec);
 
     offset += sprintf(buf + offset, "Stat-Thread-Id:: %d\r\n",
                       t_stats->id);
@@ -26,7 +25,7 @@ int append_stats(char* buf, threads_stats t_stats, struct timeval arrival, struc
 
     offset += sprintf(buf + offset, "Stat-Thread-Dynamic:: %d\r\n",
                       t_stats->dynm_req);
-    offset += sprintf(buf + offset, "Stat-Thread-Post:: %d\r\n",
+    offset += sprintf(buf + offset, "Stat-Thread-Post:: %d\r\n\r\n",
                       t_stats->post_req);
     return offset;
 }
@@ -34,46 +33,45 @@ int append_stats(char* buf, threads_stats t_stats, struct timeval arrival, struc
 // requestError(      fd,    filename,        "404",    "Not found", "OS-HW3 Server could not find this file");
 void requestError(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg, struct timeval arrival, struct timeval dispatch, threads_stats t_stats)
 {
-	char buf[MAXLINE], body[MAXBUF];
+    char buf[MAXLINE], body[MAXBUF];
 
-	// Create the body of the error message
-	sprintf(body, "<html><title>OS-HW3 Error</title>");
-	sprintf(body, "%s<body bgcolor=""fffff"">\r\n", body);
-	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
-	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-	sprintf(body, "%s<hr>OS-HW3 Web Server\r\n", body);
+    // Create the body of the error message
+    sprintf(body, "<html><title>OS-HW3 Error</title>");
+    sprintf(body, "%s<body bgcolor=""fffff"">\r\n", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr>OS-HW3 Web Server\r\n", body);
 
-	// Write out the header information for this response
-	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
-	Rio_writen(fd, buf, strlen(buf));
-	printf("%s", buf);
+    // Write out the header information for this response
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    printf("%s", buf);
 
-	sprintf(buf, "Content-Type: text/html\r\n");
-	Rio_writen(fd, buf, strlen(buf));
-	printf("%s", buf);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    printf("%s", buf);
 
-	sprintf(buf, "Content-Length: %lu\r\n", strlen(body));
+    sprintf(buf, "Content-Length: %lu\r\n", strlen(body));
 
     int buf_len = append_stats(buf, t_stats, arrival, dispatch);
-    buf_len += sprintf(buf + buf_len, "\r\n");
 
     Rio_writen(fd, buf, buf_len);
-	printf("%s", buf);
-	Rio_writen(fd, body, strlen(body));
-	printf("%s", body);
+    printf("%s", buf);
+    Rio_writen(fd, body, strlen(body));
+    printf("%s", body);
 
 }
 
 
 void requestReadhdrs(rio_t *rp)
 {
-	char buf[MAXLINE];
+    char buf[MAXLINE];
 
-	Rio_readlineb(rp, buf, MAXLINE);
-	while (strcmp(buf, "\r\n")) {
-		Rio_readlineb(rp, buf, MAXLINE);
-	}
-	return;
+    Rio_readlineb(rp, buf, MAXLINE);
+    while (strcmp(buf, "\r\n")) {
+        Rio_readlineb(rp, buf, MAXLINE);
+    }
+    return;
 }
 
 //
@@ -82,31 +80,31 @@ void requestReadhdrs(rio_t *rp)
 //
 int requestParseURI(char *uri, char *filename, char *cgiargs)
 {
-	char *ptr;
-	if (strstr(uri, "..")) {
-		sprintf(filename, "./public/home.html");
-		return 1;
-	}
-	if (!strstr(uri, "cgi")) {
-		// static
-		strcpy(cgiargs, "");
-		sprintf(filename, "./public/%s", uri);
-		if (uri[strlen(uri)-1] == '/') {
-			strcat(filename, "home.html");
-		}
-		return 1;
-	} else {
-		// dynamic
-		ptr = index(uri, '?');
-		if (ptr) {
-			strcpy(cgiargs, ptr+1);
-			*ptr = '\0';
-		} else {
-			strcpy(cgiargs, "");
-		}
-		sprintf(filename, "./public/%s", uri);
-		return 0;
-	}
+    char *ptr;
+    if (strstr(uri, "..")) {
+        sprintf(filename, "./public/home.html");
+        return 1;
+    }
+    if (!strstr(uri, "cgi")) {
+        // static
+        strcpy(cgiargs, "");
+        sprintf(filename, "./public/%s", uri);
+        if (uri[strlen(uri)-1] == '/') {
+            strcat(filename, "home.html");
+        }
+        return 1;
+    } else {
+        // dynamic
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr+1);
+            *ptr = '\0';
+        } else {
+            strcpy(cgiargs, "");
+        }
+        sprintf(filename, "./public/%s", uri);
+        return 0;
+    }
 }
 
 //
@@ -114,93 +112,89 @@ int requestParseURI(char *uri, char *filename, char *cgiargs)
 //
 void requestGetFiletype(char *filename, char *filetype)
 {
-	if (strstr(filename, ".html"))
-		strcpy(filetype, "text/html");
-	else if (strstr(filename, ".gif"))
-		strcpy(filetype, "image/gif");
-	else if (strstr(filename, ".jpg"))
-		strcpy(filetype, "image/jpeg");
-	else
-		strcpy(filetype, "text/plain");
+    if (strstr(filename, ".html"))
+        strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif"))
+        strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".jpg"))
+        strcpy(filetype, "image/jpeg");
+    else
+        strcpy(filetype, "text/plain");
 }
 
 void requestServeDynamic(int fd, char *filename, char *cgiargs, struct timeval arrival, struct timeval dispatch, threads_stats t_stats)
 {
-    //update the thread stats
     t_stats->dynm_req++;
 
-	char buf[MAXLINE], *emptylist[] = {NULL};
+    char buf[MAXBUF], *emptylist[] = {NULL};
+    int offset = 0;
 
-	// The server does only a little bit of the header.
-	// The CGI script has to finish writing out the header.
-	sprintf(buf, "HTTP/1.0 200 OK\r\n");
-	sprintf(buf, "%sServer: OS-HW3 Web Server\r\n", buf);
-    int buf_len = append_stats(buf, t_stats, arrival, dispatch);
+    offset += sprintf(buf + offset, "HTTP/1.0 200 OK\r\n");
+    offset += sprintf(buf + offset, "Server: OS-HW3 Web Server\r\n");
+    offset = append_stats(buf, t_stats, arrival, dispatch);
 
-    Rio_writen(fd, buf, buf_len);
-   	int pid = 0;
-   	if ((pid = Fork()) == 0) {
-     	 /* Child process */
-     	 Setenv("QUERY_STRING", cgiargs, 1);
-     	 /* When the CGI process writes to stdout, it will instead go to the socket */
-     	 Dup2(fd, STDOUT_FILENO);
-     	 Execve(filename, emptylist, environ);
-   	}
-  	WaitPid(pid, NULL, WUNTRACED);
+    Rio_writen(fd, buf, offset);
 
+    int pid = Fork();
+    if (pid == 0) {
+        Setenv("QUERY_STRING", cgiargs, 1);
+        Dup2(fd, STDOUT_FILENO);
+        Execve(filename, emptylist, environ);
+    }
+    WaitPid(pid, NULL, 0);
 }
+
 
 
 void requestServeStatic(int fd, char *filename, int filesize, struct timeval arrival, struct timeval dispatch, threads_stats t_stats)
 {
-    //update the thread stats
+    // update the thread stats
     t_stats->stat_req++;
 
-	int srcfd;
-	char *srcp, filetype[MAXLINE], buf[MAXBUF];
+    int srcfd;
+    char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
-	requestGetFiletype(filename, filetype);
+    requestGetFiletype(filename, filetype);
 
-	srcfd = Open(filename, O_RDONLY, 0);
+    srcfd = Open(filename, O_RDONLY, 0);
 
-	// Rather than call read() to read the file into memory,
-	// which would require that we allocate a buffer, we memory-map the file
-	srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-	Close(srcfd);
+    // memory-map the file
+    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    Close(srcfd);
 
-	// put together response
-	sprintf(buf, "HTTP/1.0 200 OK\r\n");
-	sprintf(buf, "%sServer: OS-HW3 Web Server\r\n", buf);
-	sprintf(buf, "%sContent-Length: %d\r\n", buf, filesize);
-	sprintf(buf, "%sContent-Type: %s\r\n", buf, filetype);
-    int buf_len = append_stats(buf, t_stats, arrival, dispatch);
-    buf_len += sprintf(buf + buf_len, "\r\n");
-    Rio_writen(fd, buf, buf_len);
+    // safely build the header using offset to avoid overlap
+    int offset = 0;
+    offset += sprintf(buf + offset, "HTTP/1.0 200 OK\r\n");
+    offset += sprintf(buf + offset, "Server: OS-HW3 Web Server\r\n");
+    offset += sprintf(buf + offset, "Content-Length: %d\r\n", filesize);
+    offset += sprintf(buf + offset, "Content-Type: %s\r\n", filetype);
+    offset = append_stats(buf, t_stats, arrival, dispatch);  // continues writing at the end
 
-	//  Writes out to the client socket the memory-mapped file
-	Rio_writen(fd, srcp, filesize);
-	Munmap(srcp, filesize);
-
+    Rio_writen(fd, buf, offset);
+    Rio_writen(fd, srcp, filesize);
+    Munmap(srcp, filesize);
 }
 
-void requestServePost(int fd,  struct timeval arrival, struct timeval dispatch, threads_stats t_stats, server_log log)
+
+void requestServePost(int fd, struct timeval arrival, struct timeval dispatch, threads_stats t_stats, server_log log)
 {
-    //update the thread stats
     t_stats->post_req++;
+
     char header[MAXBUF], *body = NULL;
     int body_len = get_log(log, &body);
-    // put together response
-    sprintf(header, "HTTP/1.0 200 OK\r\n");
-    sprintf(header, "%sServer: OS-HW3 Web Server\r\n", header);
-    sprintf(header, "%sContent-Length: %d\r\n", header, body_len);
-    sprintf(header, "%sContent-Type: %s\r\n", header, "text/plain");
-    int header_len = append_stats(header, t_stats, arrival, dispatch);
-    header_len += sprintf(header + header_len, "\r\n");
-    Rio_writen(fd, header, header_len);
+    int offset = 0;
+
+    offset += sprintf(header + offset, "HTTP/1.0 200 OK\r\n");
+    offset += sprintf(header + offset, "Server: OS-HW3 Web Server\r\n");
+    offset += sprintf(header + offset, "Content-Length: %d\r\n", body_len);
+    offset += sprintf(header + offset, "Content-Type: text/plain\r\n");
+    offset = append_stats(header, t_stats, arrival, dispatch);
+
+    Rio_writen(fd, header, offset);
     Rio_writen(fd, body, body_len);
     free(body);
-
 }
+
 
 // handle a request
 void requestHandle(int fd, struct timeval arrival, struct timeval dispatch, threads_stats t_stats, server_log log)
@@ -240,6 +234,7 @@ void requestHandle(int fd, struct timeval arrival, struct timeval dispatch, thre
             requestServeStatic(fd, filename, sbuf.st_size, arrival, dispatch, t_stats);
 
             char statBuffer[MAXBUF];
+            statBuffer[0] = '\0';
             int statLength = append_stats(statBuffer, t_stats, arrival, dispatch);
             add_to_log(log, statBuffer, statLength);
 
@@ -254,16 +249,13 @@ void requestHandle(int fd, struct timeval arrival, struct timeval dispatch, thre
             requestServeDynamic(fd, filename, cgiargs, arrival, dispatch, t_stats);
 
             char statBuffer[MAXBUF];
+            statBuffer[0] = '\0';
             int statLength = append_stats(statBuffer, t_stats, arrival, dispatch);
             add_to_log(log, statBuffer, statLength);
         }
 
     } else if (!strcasecmp(method, "POST")) {
         requestServePost(fd, arrival, dispatch, t_stats, log);
-
-        char statBuffer[MAXBUF];
-        int statLength = append_stats(statBuffer, t_stats, arrival, dispatch);
-        add_to_log(log, statBuffer, statLength);
 
     } else {
         requestError(fd, method, "501", "Not Implemented",
